769959e294b170371b8b041c7241260d649e551f
Setareh Arzpour <arzpourr@gmail.com>
2026-02-11 17:04:25 +0330

    Edit cheque by id - add modal to show the previous data and edit - fix cheque form

--- apis/mutations/cheques.ts
+++ apis/mutations/cheques.ts
@@ -1,5 +1,8 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { getChequesByDeal, getChequesByVin } from "../client/cheques";
+import { toast } from "sonner";
+import { updateCheque } from "../client/chequesNew";
+import { IChequeNew } from "@/types/new-backend-types";
 
 export const useGetChequesByDealId = () => {
   return useMutation({
@@ -12,5 +15,24 @@ export const useGetChequesByVin = () => {
   return useMutation({
     mutationKey: ["get-cheques-by-vin"],
     mutationFn: getChequesByVin,
+  });
+};
+
+export const useUpdateCheque = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, data }: { id: string; data: Partial<IChequeNew> }) =>
+      updateCheque(id, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["get-all-cheques"] });
+      queryClient.invalidateQueries({ queryKey: ["get-cheque-by-id"] });
+      toast.success("معامله با موفقیت به‌روزرسانی شد");
+    },
+    onError: (error: any) => {
+      toast.error(
+        error?.response?.data?.message || "خطا در به‌روزرسانی ",
+      );
+    },
   });
 };
